e40403d718c4938d340fd9a694ff7c1968f4d9bf
yixiangD <doggydeng@gmail.com>
2023-03-04 00:50:19 -0500

    start from melih's version

--- ROC_ML.R
+++ ROC_ML.R
@@ -1,4 +1,3 @@
-
 library(dplyr)
 library(ROCR)
 library(ggplot2)
@@ -7,97 +6,94 @@ library(klaR)
 library(stringr)
 library(caret)
 
-ROC_Anfin4 <- function(data, dependent = "Dependent",   k=5){
-  
+ROC_Anfin4 <- function(data, dependent = "Dependent", k = 5) {
   stopifnot(!missing(data))
   #----------------------------------------------------------------------------
   col_name <- colnames(data)
-  moveYtoEnd<-function(df,  dependent ){ 
-    
-    yvar<-dependent
+  moveYtoEnd <- function(df, dependent) {
+    yvar <- dependent
     cnames <- colnames(df)
     idx <- which(cnames == yvar)
     idxs <- which(cnames != yvar)
     orderedNames <- c(cnames[idxs], cnames[idx])
-    df <- subset(df, select=orderedNames)
+    df <- subset(df, select = orderedNames)
     return(df)
   }
-  data<-moveYtoEnd(data,  dependent) # sort of the dependent variable
-  
-  colnames(data)[dim(data)[2]]<-"Dependent"
-  data$Dependent <- factor(data$Dependent)
-  
-  lvls = levels(data$Dependent)
-  aucs = c()
-  acc_need<-  c("Sensitivity", "Specificity", "PPV", "NPV","ACC", "AUC")
-  all_res <- matrix(NA, nrow = (k), ncol=length(acc_need))
-  colnames(all_res)<-acc_need
- 
-  rownames(all_res)<-  paste0("row",seq(1:k))
-  
-  folds <- createFolds(data$Dependent, k = k)
-  type.id<-1
-  
-  for (i in 1:k) {
+  data <- moveYtoEnd(data, dependent) # sort of the dependent variable
 
+  colnames(data)[dim(data)[2]] <- "Dependent"
+  data$Dependent <- factor(data$Dependent)
+
+  lvls <- levels(data$Dependent)
+  aucs <- c()
+  acc_need <- c("Sensitivity", "Specificity", "PPV", "NPV", "ACC", "AUC")
+  all_res <- matrix(NA, nrow = (k), ncol = length(acc_need))
+  colnames(all_res) <- acc_need
+
+  rownames(all_res) <- paste0("row", seq(1:k))
+
+  folds <- createFolds(data$Dependent, k = k)
+  type.id <- 1
+
+  for (i in 1:k) {
     testIndex <- folds[[i]]
-    
-    data_train <- data[-testIndex,]
-    data_test <-  data[testIndex,]
-    
-    data1<-data_train
-    
-    data1$Dependent    =  as.factor(data1$Dependent == lvls[type.id])
+
+    data_train <- data[-testIndex, ]
+    data_test <- data[testIndex, ]
+
+    data1 <- data_train
+
+    data1$Dependent <- as.factor(data1$Dependent == lvls[type.id])
     mytarget <- "Dependent"
-    
-    myFormula <- as.formula(paste(paste(mytarget,"~"), "."))
-    nbmodel       =  NaiveBayes(myFormula, data=data1)
-    resp<-which(colnames(data1)=="Dependent")
-    
-    nbprediction  =  predict(nbmodel, newdata = data_test, type='raw')
-    score = nbprediction$posterior[, 'TRUE']
-    actual.class = data_test$Dependent == lvls[type.id]
-    
-    pred   = prediction(score, actual.class)
-    nbperf = performance(pred, "tpr", "fpr")
-    
-    roc.x = unlist(nbperf@x.values)
-    roc.y = unlist(nbperf@y.values)
-   
-    #AUC
-    nbauc = performance(pred, "auc")
-    nbauc = unlist(slot(nbauc, "y.values"))
-    aucs[type.id] = nbauc
-    #Sens 
-    nbsens = performance(pred, "sens")
-    nbsens = unlist(slot(nbsens, "y.values"))
-    #Spec
-    nbspec = performance(pred, "spec")
-    nbspec = unlist(slot(nbspec, "y.values"))
-    #PPV
-    nbppv = performance(pred, "ppv")
-    nbppv = unlist(slot(nbppv, "y.values"))
-    #nPV
-    nbnpv = performance(pred, "npv")
-    nbnpv = unlist(slot(nbnpv, "y.values"))
-    #acc
-    nbacc = performance(pred, "acc")
-    nbacc = unlist(slot(nbacc, "y.values"))
-    # G-means  
-    th<-which.max(sqrt(nbsens*nbspec))
-    
+
+    myFormula <- as.formula(paste(paste(mytarget, "~"), "."))
+    nbmodel <- NaiveBayes(myFormula, data = data1)
+    resp <- which(colnames(data1) == "Dependent")
+
+    nbprediction <- predict(nbmodel, newdata = data_test, type = "raw")
+    score <- nbprediction$posterior[, "TRUE"]
+    actual.class <- data_test$Dependent == lvls[type.id]
+
+    pred <- prediction(score, actual.class)
+    nbperf <- performance(pred, "tpr", "fpr")
+
+    roc.x <- unlist(nbperf@x.values)
+    roc.y <- unlist(nbperf@y.values)
+
+    # AUC
+    nbauc <- performance(pred, "auc")
+    nbauc <- unlist(slot(nbauc, "y.values"))
+    aucs[type.id] <- nbauc
+    # Sens
+    nbsens <- performance(pred, "sens")
+    nbsens <- unlist(slot(nbsens, "y.values"))
+    # Spec
+    nbspec <- performance(pred, "spec")
+    nbspec <- unlist(slot(nbspec, "y.values"))
+    # PPV
+    nbppv <- performance(pred, "ppv")
+    nbppv <- unlist(slot(nbppv, "y.values"))
+    # nPV
+    nbnpv <- performance(pred, "npv")
+    nbnpv <- unlist(slot(nbnpv, "y.values"))
+    # acc
+    nbacc <- performance(pred, "acc")
+    nbacc <- unlist(slot(nbacc, "y.values"))
+    # G-means
+    th <- which.max(sqrt(nbsens * nbspec))
+
     # "Sensitivity","Specificity","PPV","NPV","AUC"
-    all_res[i,]<-round(c(nbsens[th], nbspec[th], nbppv[th],nbnpv[th], nbacc[th], nbauc),4)
-    #all_res<-all_res2[i,]
-    
+    all_res[i, ] <- round(c(nbsens[th], nbspec[th], nbppv[th], nbnpv[th], nbacc[th], nbauc), 4)
+    # all_res<-all_res2[i,]
+
     # if(plot == TRUE){
     #   nm<- paste0("ROC_pict", ".jpg")
     #   dev.copy(jpeg, filename= nm);
     # }
-    nm1<-paste0("ROC_acc", ".csv")
+    nm1 <- paste0("ROC_acc", ".csv")
     # write.csv(all_res, nm1)
-  }# end of for loop
-  resFin<-apply(all_res, 2, mean)
-  
-  return(list(nFold_accuracy=  resFin))
+  } # end of for loop
+  resFin <- apply(all_res, 2, mean)
+
+  return(list(nFold_accuracy = resFin))
 }
--- classic_ROC.R
+++ classic_ROC.R
@@ -1,133 +1,147 @@
-library(cutpointr )
+library(cutpointr)
 library(pROC)
 
 
 ROC_fin <- function(
-    data, 
+    data,
     x,
     y,
     method = c("best", "cutoff", "maximized"),
     index = NULL,
-    cutoff = NULL){
-  
+    cutoff = NULL) {
   # if the method is best you can specify the index = c("youden","closest.topleft")
   # cutoff = NULL #you can specify the cutoff
   # data = aSAH
   # x="s100b"
   # y="outcome"
   # index = "youden"
-  x1<- data[[x]]
-  y1<- data[[y]]
-  
-  roc.s100b <- roc(y1, x1, ci=TRUE) 
-  auc.s100b <- round(roc.s100b$auc[1],4)
-  ci_L<-round(roc.s100b$ci[1],4)
-  ci_U<-round(roc.s100b$ci[3],4)
-  
-  if(method == "best"){
-    main_res<-round(coords(roc.s100b, "best", ret=c("threshold",  "sensitivity", "specificity", 
-                                                    "ppv", "npv", "accuracy", "auc"),  
-                           best.method = index, transpose = FALSE),4)
-    main_res= cbind(main_res, auc = auc.s100b, ci_low=ci_L, ci_up=ci_U)
-    
-    main_res = list(best_res = main_res, pl = roc.s100b)
-    
-  }else if(method == "cutoff"){
-    
-    main_res<-round(coords(roc.s100b, cutoff, ret=c("threshold",  "sensitivity", "specificity", 
-                                                    "ppv", "npv", "accuracy"),  
-                           best.method = index, transpose = FALSE),4)
-    main_res= cbind(main_res, auc = auc.s100b, ci_low=ci_L, ci_up=ci_U)
-    main_res = list(specific_cutoff = main_res, pl = roc.s100b)
-  }else{
-    
-    min_cons<-c(0.1, 0.25, 0.5, 0.7)
-    
-    main_res <- data.frame(cut_off = numeric(),
-                           Sen = numeric(),
-                           Spec = numeric(),
-                           PPV = numeric(),
-                           NPV = numeric(),
-                           Acc = numeric(),
-                           Minimum_Specificity = numeric())
-    
-    
+  x1 <- data[[x]]
+  y1 <- data[[y]]
+
+  roc.s100b <- roc(y1, x1, ci = TRUE)
+  auc.s100b <- round(roc.s100b$auc[1], 4)
+  ci_L <- round(roc.s100b$ci[1], 4)
+  ci_U <- round(roc.s100b$ci[3], 4)
+
+  if (method == "best") {
+    main_res <- round(coords(roc.s100b, "best",
+      ret = c(
+        "threshold", "sensitivity", "specificity",
+        "ppv", "npv", "accuracy", "auc"
+      ),
+      best.method = index, transpose = FALSE
+    ), 4)
+    main_res <- cbind(main_res, auc = auc.s100b, ci_low = ci_L, ci_up = ci_U)
+
+    main_res <- list(best_res = main_res, pl = roc.s100b)
+  } else if (method == "cutoff") {
+    main_res <- round(coords(roc.s100b, cutoff,
+      ret = c(
+        "threshold", "sensitivity", "specificity",
+        "ppv", "npv", "accuracy"
+      ),
+      best.method = index, transpose = FALSE
+    ), 4)
+    main_res <- cbind(main_res, auc = auc.s100b, ci_low = ci_L, ci_up = ci_U)
+    main_res <- list(specific_cutoff = main_res, pl = roc.s100b)
+  } else {
+    min_cons <- c(0.1, 0.25, 0.5, 0.7)
+
+    main_res <- data.frame(
+      cut_off = numeric(),
+      Sen = numeric(),
+      Spec = numeric(),
+      PPV = numeric(),
+      NPV = numeric(),
+      Acc = numeric(),
+      Minimum_Specificity = numeric()
+    )
+
+
     for (ix in 1:length(min_cons)) {
       # constrain specif
-      df_ns<-data.frame(Dependent= data[,y], X1 = data[,x])
+      df_ns <- data.frame(Dependent = data[, y], X1 = data[, x])
       df_ns <- df_ns[complete.cases(df_ns), ]
-      
-      cp <- cutpointr( x = df_ns[,"X1"], class = df_ns[,"Dependent"],
-                       method = maximize_metric,
-                       metric = sens_constrain,
-                       constrain_metric = specificity,
-                       min_constrain = min_cons[ix])
-      test<-summary(cp)
-      cutf<-test$confusion_matrix[[1]][[1]]#cutoff
-      tp<-test$confusion_matrix[[1]][[2]]
-      fn<-test$confusion_matrix[[1]][[3]]
-      fp<-test$confusion_matrix[[1]][[4]]
-      tn<-test$confusion_matrix[[1]][[5]]
-      sens<-(tp)/(tp+fn) 
-      spec<-(tn)/(tn+fp) 
-      PPV<-(tp)/(tp+fp)
-      NPV<-(tn)/(tn+fn)
-      Acc = (tp+tn)/(tp+fp+tn+fn)
-      
-      main_res[ix,1] <- cutf
-      main_res[ix,2] <- sens
-      main_res[ix,3] <- spec
-      main_res[ix,4] <- PPV
-      main_res[ix,5] <- NPV
-      main_res[ix,6] <- Acc
-      main_res[ix,7] <- min_cons[ix]
-      round(main_res,4)
-      main_res_max_sens<-main_res
+
+      cp <- cutpointr(
+        x = df_ns[, "X1"], class = df_ns[, "Dependent"],
+        method = maximize_metric,
+        metric = sens_constrain,
+        constrain_metric = specificity,
+        min_constrain = min_cons[ix]
+      )
+      test <- summary(cp)
+      cutf <- test$confusion_matrix[[1]][[1]] # cutoff
+      tp <- test$confusion_matrix[[1]][[2]]
+      fn <- test$confusion_matrix[[1]][[3]]
+      fp <- test$confusion_matrix[[1]][[4]]
+      tn <- test$confusion_matrix[[1]][[5]]
+      sens <- (tp) / (tp + fn)
+      spec <- (tn) / (tn + fp)
+      PPV <- (tp) / (tp + fp)
+      NPV <- (tn) / (tn + fn)
+      Acc <- (tp + tn) / (tp + fp + tn + fn)
+
+      main_res[ix, 1] <- cutf
+      main_res[ix, 2] <- sens
+      main_res[ix, 3] <- spec
+      main_res[ix, 4] <- PPV
+      main_res[ix, 5] <- NPV
+      main_res[ix, 6] <- Acc
+      main_res[ix, 7] <- min_cons[ix]
+      round(main_res, 4)
+      main_res_max_sens <- main_res
     }
-    
-    main_res <- data.frame(cut_off = numeric(),
-                           Sen = numeric(),
-                           Spec = numeric(),
-                           PPV = numeric(),
-                           NPV = numeric(),
-                           Acc = numeric(),
-                           Minimum_Sensitivity = numeric())
-    
+
+    main_res <- data.frame(
+      cut_off = numeric(),
+      Sen = numeric(),
+      Spec = numeric(),
+      PPV = numeric(),
+      NPV = numeric(),
+      Acc = numeric(),
+      Minimum_Sensitivity = numeric()
+    )
+
     for (ix in 1:length(min_cons)) {
       # constrain specif
-      df_ns<-data.frame(Dependent= data[,y], X1 = data[,x])
+      df_ns <- data.frame(Dependent = data[, y], X1 = data[, x])
       df_ns <- df_ns[complete.cases(df_ns), ]
-      
-      cp <- cutpointr(x = df_ns[,"X1"], class = df_ns[,"Dependent"],
-                      method = maximize_metric,
-                      metric = spec_constrain,
-                      constrain_metric = sensitivity,
-                      min_constrain = min_cons[ix])
-      test<-summary(cp)
-      cutf<-test$confusion_matrix[[1]][[1]]#cutoff
-      tp<-test$confusion_matrix[[1]][[2]]
-      fn<-test$confusion_matrix[[1]][[3]]
-      fp<-test$confusion_matrix[[1]][[4]]
-      tn<-test$confusion_matrix[[1]][[5]]
-      sens<-(tp)/(tp+fn) 
-      spec<-(tn)/(tn+fp) 
-      PPV<-(tp)/(tp+fp)
-      NPV<-(tn)/(tn+fn)
-      Acc = (tp+tn)/(tp+fp+tn+fn)
-      
-      main_res[ix,1] <- cutf
-      main_res[ix,2] <- sens
-      main_res[ix,3] <- spec
-      main_res[ix,4] <- PPV
-      main_res[ix,5] <- NPV
-      main_res[ix,6] <- Acc
-      main_res[ix,7] <- min_cons[ix]
-      round(main_res,4)
-      main_res_max_spec<-main_res
+
+      cp <- cutpointr(
+        x = df_ns[, "X1"], class = df_ns[, "Dependent"],
+        method = maximize_metric,
+        metric = spec_constrain,
+        constrain_metric = sensitivity,
+        min_constrain = min_cons[ix]
+      )
+      test <- summary(cp)
+      cutf <- test$confusion_matrix[[1]][[1]] # cutoff
+      tp <- test$confusion_matrix[[1]][[2]]
+      fn <- test$confusion_matrix[[1]][[3]]
+      fp <- test$confusion_matrix[[1]][[4]]
+      tn <- test$confusion_matrix[[1]][[5]]
+      sens <- (tp) / (tp + fn)
+      spec <- (tn) / (tn + fp)
+      PPV <- (tp) / (tp + fp)
+      NPV <- (tn) / (tn + fn)
+      Acc <- (tp + tn) / (tp + fp + tn + fn)
+
+      main_res[ix, 1] <- cutf
+      main_res[ix, 2] <- sens
+      main_res[ix, 3] <- spec
+      main_res[ix, 4] <- PPV
+      main_res[ix, 5] <- NPV
+      main_res[ix, 6] <- Acc
+      main_res[ix, 7] <- min_cons[ix]
+      round(main_res, 4)
+      main_res_max_spec <- main_res
     }
-    main_res =    list(maximum_sensitivity = main_res_max_sens, 
-                       maximum_specificity = main_res_max_spec, 
-                       pl=roc.s100b)
+    main_res <- list(
+      maximum_sensitivity = main_res_max_sens,
+      maximum_specificity = main_res_max_spec,
+      pl = roc.s100b
+    )
   }
   return(main_res)
 }
