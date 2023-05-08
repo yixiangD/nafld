f431d75fdab9a1f1527437038e464d55687c7370
yixiangD <doggydeng@gmail.com>
2023-05-08 15:31:27 -0400

    add input output

--- main2.R
+++ main2.R
@@ -5,4 +5,19 @@ data <- data[, !duplicated(colnames(data))]
 outdir <- "./results"
 summ <- psych::describe(data)
 summ <- cbind(" " = rownames(summ), summ)
-writexl::write_xlsx(summ, paste(outdir, "data_summary.xlsx", sep = "/"))
+
+input <- c("HSI", "aHSI", "FLI", "AST_ALT", "ALT_AST", "LAP", "TYG", "ION", "FIB4", "NFS", "APRI", "LFS")
+input_in_table <- c("HSI", "aHSI", "FLI", "AST_ALT", "ALT_AST", "LAPIndex", "TyG index", "ION", "FIB-4", "NFS", "APRI", "NAFLD-LFS")
+# Control vs all
+out1 <- "control_case" # derive from column named "Group Class", merge lean & obese, merge NAFL & NASH
+# NAFL VS NASH
+out2 <- "NAFL_NASH" # also from "Group Class"
+# FIBROSIS F0-1 VS F2-4
+out3 <- "FIBROSIS01" # column named Fibrosis01vs23
+# FIBROSIS F0-2 VS F3-4
+out4 <- "FIBROSIS012" # column named Fibrosis012vs3
+# PRESENCE VS ABSENCE OF BALOONINNG
+out5 <- "BALOONING" # column named AnyBallooning
+# PRESENCE VS ABSENCE OF INFLAMMATION
+out6 <- "INFLAMMATION" # column named anyinflammation
+# writexl::write_xlsx(summ, paste(outdir, "data_summary.xlsx", sep = "/"))
