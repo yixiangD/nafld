5c873ef93ffd5b7f3dbbf9750a4324fe0c276e84
yixiangD <doggydeng@gmail.com>
2023-05-09 11:35:27 -0400

    begin test results

--- main2.R
+++ main2.R
@@ -7,9 +7,10 @@ summ <- psych::describe(data)
 summ <- cbind(" " = rownames(summ), summ)
 
 input <- c("HSI", "aHSI", "FLI", "AST_ALT", "ALT_AST", "LAP", "TYG", "ION", "FIB4", "NFS", "APRI", "LFS")
-input_in_table <- c("HSI", "aHSI", "FLI", "AST_ALT", "ALT_AST", "LAPIndex", "TyG index", "ION", "FIB-4", "NFS", "APRI", "NAFLD-LFS")
+input_in_table <- c("HSI", "aHSI", "FLI", "AST_ALT", "ALT_AST", "LAPIndex", "TyG index", "ION", "FIB-4", "NAFLD Fibrosis Score", "APRI", "NAFLD-LFS")
+colnames(data) <- sapply(colnames(data), function(x) trimws(x))
 # Control vs all
-out1 <- "control_case" # derive from column named "Group Class", merge lean & obese, merge NAFL & NASH
+out1 <- "ControlsVsAll" # derive from column named "Group Class", merge lean & obese, merge NAFL & NASH
 # NAFL VS NASH
 out2 <- "NAFL_NASH" # also from "Group Class"
 # FIBROSIS F0-1 VS F2-4
@@ -23,10 +24,13 @@ out6 <- "INFLAMMATION" # column named anyinflammation
 # writexl::write_xlsx(summ, paste(outdir, "data_summary.xlsx", sep = "/"))
 
 # create out variable 1
-data[, out1] <- ifelse(grepl("Control", data$`Group Class`), "control", "case")
+# data[, out1] <- ifelse(grepl("Control", data$`Group Class`), "control", "case")
 # use this for out variable 2
 data2 <- data[!grepl("Control", data$`Group Class`), ]
 
 geo.grp <- "MED" # "AUS", "ALL"
 geo.col <- "Database0Mediterranean1Australian"
 in_var <- input_in_table[1]
+source("classic_ROC.R")
+res <- ROC_fin(data[data[[geo.col]] == 1, ], in_var, out1, "maximized")
+print(res)
