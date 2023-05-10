6e1c7a78277cd865bd1c8746ed29699ae70f860b
yixiangD <doggydeng@gmail.com>
2023-05-10 17:49:16 -0400

    update file for new dataset

--- main2.R
+++ main2.R
@@ -1,4 +1,4 @@
-data <- readxl::read_excel("data/NAFLD April 2023 Australia and Mediterranean Database (Laura Valenzuela).xlsx")
+data <- readxl::read_excel("data/OK. Yixiang_Merged database_Med_Austr.xlsx")
 # remove duplicated columns, only keep one replicate
 colnames(data) <- gsub("\\.\\.\\..", "", colnames(data))
 data <- data[, !duplicated(colnames(data))]
@@ -7,16 +7,16 @@ summ <- psych::describe(data)
 summ <- cbind(" " = rownames(summ), summ)
 
 input <- c("HSI", "aHSI", "FLI", "AST_ALT", "ALT_AST", "LAP", "TYG", "ION", "FIB4", "NFS", "APRI", "LFS")
-input_in_table <- c("HSI", "aHSI", "FLI", "AST_ALT", "ALT_AST", "LAPIndex", "TyG index", "ION", "FIB-4", "NAFLD Fibrosis Score", "APRI", "NAFLD-LFS")
+input_in_table <- c("HSI", "aHSI", "FLI", "AST_ALT", "ALT_AST", "LAP", "TyGindex", "ION", "FIB4", "NAFLDFibrosisScore", "APRI", "NAFLDLFS_NEW_IDF")
 colnames(data) <- sapply(colnames(data), function(x) trimws(x))
 # Control vs all
 out1 <- "ControlsVsAll" # derive from column named "Group Class", merge lean & obese, merge NAFL & NASH
 # NAFL VS NASH
-out2 <- "controlvsNAFLvsNASH"
+out2 <- "NAFLvsNASH"
 # FIBROSIS F0-1 VS F2-4
-out3 <- "Fibrosis01vs23"
+out3 <- "F01_234"
 # FIBROSIS F0-2 VS F3-4
-out4 <- "Fibrosis012vs3"
+out4 <- "F01_2_34"
 # PRESENCE VS ABSENCE OF BALOONINNG
 out5 <- "AnyBallooning"
 # PRESENCE VS ABSENCE OF INFLAMMATION
@@ -26,7 +26,7 @@ out6 <- "anyinflammation"
 # create out variable 1
 outs <- c(out1, out2, out3, out4, out5, out6)
 
-geo.col <- "Database0Mediterranean1Australian"
+geo.col <- "Study"
 source("classic_ROC.R")
 
 gen_res_table <- function(df, input_in_table, out) {
@@ -54,9 +54,9 @@ gen_res_table <- function(df, input_in_table, out) {
 
 for (geo.grp in c("AUS", "MED", "ALL")) {
   if (geo.grp == "MED") {
-    df <- data[data[[geo.col]] == 0, ]
-  } else if (geo.grp == "AUS") {
     df <- data[data[[geo.col]] == 1, ]
+  } else if (geo.grp == "AUS") {
+    df <- data[data[[geo.col]] == 2, ]
   } else {
     df <- data
   }
@@ -64,13 +64,13 @@ for (geo.grp in c("AUS", "MED", "ALL")) {
   for (out in outs) {
     # use this for out variable 2
     if (out == out2) {
-      df.loc <- df[df[[out]] > 0, ]
+      df.loc <- df[!is.na(df[[out]]), ]
     } else {
       df.loc <- df
       df.loc[[out]] <- ifelse(df.loc[[out]] > 0, 1, 0)
     }
-    # print(out)
-    # print(unique(df.loc[[out]]))
+    print(df.loc)
+    print(unique(df.loc[[out]]))
     res <- gen_res_table(df.loc, input_in_table, out)
     writexl::write_xlsx(res$sens, paste0("results/", geo.grp, out, "_sens.xlsx"))
     writexl::write_xlsx(res$spec, paste0("results/", geo.grp, out, "_spec.xlsx"))
